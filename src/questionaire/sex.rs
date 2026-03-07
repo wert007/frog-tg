1b6de290d9b2f0a6b2bf204d5b7f22f63adc4b41
wert007 <pablohamacher@web.de>
2026-03-07 16:58:15 +0100

    Add teichmolch and improve interface a tiny bit

--- src/questionaire/sex.rs
+++ src/questionaire/sex.rs
@@ -1,7 +1,7 @@
 use anyhow::bail;
 use teloxide::{dispatching::dialogue::InMemStorage, prelude::*, types::InputPollOption};
 
-use crate::{CompleteWalk, PollExt, Sex, State};
+use crate::{PollExt, Sex, State};
 
 pub async fn erdkroete(
     bot: Bot,
@@ -23,25 +23,13 @@ pub async fn erdkroete(
     Ok(())
 }
 
-pub(crate) async fn erdkroete_answered(
-    dialoge: Dialogue<State, InMemStorage<State>>,
-    walk: CompleteWalk,
-    poll: Poll,
-) -> Result<(), anyhow::Error> {
-    let sex = match poll.selected_index() {
+pub(crate) async fn erdkroete_answered(poll: Poll) -> Result<Sex, anyhow::Error> {
+    Ok(match poll.selected_index() {
         -1 => bail!("Unselecting not supported!"),
         0..3 => Sex::Male,
         3 => Sex::Female,
         _ => Sex::Unknown,
-    };
-    dialoge
-        .update(State::FrogIdentifiedSex {
-            name: "Erdkröte".into(),
-            walk,
-            sex,
-        })
-        .await?;
-    Ok(())
+    })
 }
 
 pub(crate) async fn gruenfrosch(
@@ -63,23 +51,42 @@ pub(crate) async fn gruenfrosch(
     Ok(())
 }
 
-pub(crate) async fn gruenfrosch_answered(
-    dialoge: Dialogue<State, InMemStorage<State>>,
-    walk: CompleteWalk,
-    poll: Poll,
-) -> Result<(), anyhow::Error> {
-    let sex = match poll.selected_index() {
+pub(crate) async fn gruenfrosch_answered(poll: Poll) -> Result<Sex, anyhow::Error> {
+    Ok(match poll.selected_index() {
         -1 => bail!("No unselecting supported!"),
         0..2 => Sex::Male,
         2 => Sex::Female,
         _ => Sex::Unknown,
-    };
-    dialoge
-        .update(State::FrogIdentifiedSex {
-            name: "Grünfrosch".into(),
-            walk,
-            sex,
-        })
-        .await?;
+    })
+}
+
+pub(crate) async fn teichmolch(
+    bot: Bot,
+    dialoge: Dialogue<State, InMemStorage<State>>,
+) -> Result<(), anyhow::Error> {
+    bot.send_poll(
+        dialoge.chat_id(),
+        "Select whichever applied",
+        [
+            "Große schwarze Punkte auf Unterseite",
+            "Kleine schwarze Punkte auf Unterseite",
+            "(kleiner) Rückenkamm vorhanden",
+            "Kein Rückenkamm vorhanden",
+            "Punkte unterbrechen orange Linie auf Schwanzunterseite",
+            "Keine Punkte auf Schwanzunterseite, durchgängige orange Linie",
+            "Unsicher/Keins davon",
+        ]
+        .map(InputPollOption::new),
+    )
+    .await?;
     Ok(())
+}
+
+pub(crate) async fn teichmolch_answered(poll: Poll) -> Result<Sex, anyhow::Error> {
+    Ok(match poll.selected_index() {
+        -1 => bail!("No unselecting supported"),
+        0 | 2 | 4 => Sex::Male,
+        1 | 3 | 5 => Sex::Female,
+        _ => Sex::Unknown,
+    })
 }
