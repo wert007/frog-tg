d3b3668ea984c34a2aae9c373a811dc9a696f10c
wert007 <pablohamacher@web.de>
2026-03-08 13:26:23 +0100

    Fill in header line

--- src/reports/pdf.rs
+++ src/reports/pdf.rs
@@ -1,10 +1,12 @@
+use std::borrow::Cow;
+
 use lopdf::{
     Document, FontData, Object, Stream,
     content::{Content, Operation},
     dictionary,
 };
 
-use crate::CompleteWalk;
+use crate::{CompleteWalk, weather::WeatherStats};
 
 const FONT: &[u8] = include_bytes!("../../assets/fonts/Coolvetica Rg.otf");
 const TEMPLATE: &[u8] = include_bytes!("../../assets/template.pdf");
@@ -17,10 +19,67 @@ pub fn create_pdf_report(walk: &CompleteWalk) -> anyhow::Result<()> {
     let page_id = *pages.get(&1).unwrap();
 
     write_date(&mut doc, page_id, walk.start)?;
+    write_weather(&mut doc, page_id, walk.weather)?;
     // write(&mut doc, "test", 12, [10, 10], page_id)?;
 
     doc.save("output.pdf")?;
 
+    Ok(())
+}
+
+fn write_weather(
+    doc: &mut Document,
+    page_id: (u32, u16),
+    weather: WeatherStats,
+) -> anyhow::Result<()> {
+    let temp = if let Some(end) = weather.temperature_end {
+        if (end - weather.temperature_start) > 3.0 {
+            format!("{} C-{} C", weather.temperature_start, end)
+        } else {
+            format!("{} C", weather.temperature_start)
+        }
+    } else {
+        format!("{} C", weather.temperature_start)
+    };
+    write(doc, temp, 12, [290, 129], page_id)?;
+    write(
+        doc,
+        weather.wind_beaufort.to_string(),
+        12,
+        [460, 129],
+        page_id,
+    )?;
+    let text = format!(
+        "{}/{}/{}",
+        match weather.ground_humidity {
+            crate::weather::GroundHumidity::Unknown => "?",
+            crate::weather::GroundHumidity::Wet => "nass",
+            crate::weather::GroundHumidity::Humid => "feucht",
+            crate::weather::GroundHumidity::Dry => "trocken",
+            crate::weather::GroundHumidity::VeryDry => "sehr trocken",
+        },
+        match weather.percipation {
+            crate::weather::Percipation::None => "keiner",
+            crate::weather::Percipation::StrongRain => "Starker Regen",
+            crate::weather::Percipation::ModerateRain => "Mäßiger Regen",
+            crate::weather::Percipation::Drizzle => "Niesel",
+            crate::weather::Percipation::Fog => "Nebel",
+            crate::weather::Percipation::Snow => "Schnee",
+            crate::weather::Percipation::Graupel => "Graupel",
+            crate::weather::Percipation::Unknown => "?",
+        },
+        match weather.cloudiness {
+            crate::weather::Cloudiness::AllClouds => "bedeckt".into(),
+            crate::weather::Cloudiness::ManyClouds => "stark bewölkt".into(),
+            crate::weather::Cloudiness::Clouds => "bewölkt".into(),
+            crate::weather::Cloudiness::FewClouds => "leicht bewölkt".into(),
+            crate::weather::Cloudiness::Clear => "wolkenlos".into(),
+            crate::weather::Cloudiness::GettingCloudy => "zuziehend".into(),
+            crate::weather::Cloudiness::GettingClear => "aufklarend".into(),
+            crate::weather::Cloudiness::Error(clouds) => Cow::Owned(format!("? (clouds: {clouds}")),
+        }
+    );
+    write(doc, text, 12, [720, 129], page_id)?;
     Ok(())
 }
 
@@ -33,7 +92,7 @@ fn write_date(
         doc,
         start.format("%d.%m.%Y").to_string(),
         12,
-        [100, 130],
+        [100, 129],
         page_id,
     )
 }
